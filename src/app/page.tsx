200518ea51a3eca6bb7afedf943b00e365fad0e1
Filip van Harreveld <filip.vanharreveld@iodigital.com>
2024-08-19 17:26:22 +0200

    feat(cleanup)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
-import styles from "./page.module.css";
+import styles from './page.module.css'
 
-export default function Home() {
-
+export default function Home () {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>Interactions overview</h1>
-      <p className={styles.description}>
-        Library of joyful interactions and animations
-      </p>
+      <div>
+        <h1 className={styles.title}>Interactions overview</h1>
+        <p className={styles.description}>
+          Library of joyful interactions and animations
+        </p>
+      </div>
       <ul className={styles.grid}>
         <li className={styles.card}>
-          <a href="/multiline-underline">multi line hover</a>
+          <a href='/multiline-underline'>Multi line hover</a>
         </li>
         <li className={styles.card}>
-          <a href="/animated-text">Animated text</a>
+          <a href='/animated-text'>Animated text</a>
         </li>
         <li className={styles.card}>
-          <a href="/mouse-follow">Mouse follow</a>
+          <a href='/mouse-follow'>Mouse follow</a>
         </li>
         <li className={styles.card}>
-          <a href="/custom-cursor">Custom cursor with multiple animation types<i>(width Zustand implementation)</i></a>
+          <a href='/custom-cursor'>
+            Custom cursor with multiple animation types
+            <i>(width Zustand implementation)</i>
+          </a>
         </li>
         <li className={styles.card}>
-          <a href="/3d-header">3D heder using react three fiber & GSAP</a>
+          <a href='/3d-header'>3D heder using react three fiber & GSAP</a>
         </li>
         <li className={styles.card}>
-          <a href="/timeline">Timeline</a>
+          <a href='/timeline'>Timeline</a>
         </li>
       </ul>
     </main>
-  );
+  )
 }
