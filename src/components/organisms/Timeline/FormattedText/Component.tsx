47ee806f7dae5c9ea740b18072772de7af32bb17
Filip van Harreveld <filip.vanharreveld@iodigital.com>
2024-08-28 10:17:40 +0200

    underline

--- src/components/organisms/Timeline/FormattedText/Component.tsx
+++ src/components/organisms/Timeline/FormattedText/Component.tsx
@@ -7,23 +7,28 @@ interface Props {
   className?: string
 }
 
-export const FormattedText = React.forwardRef((props: Props, ref: React.Ref<HTMLDivElement>): JSX.Element => {
-  const SANITIZE_CONFIG = {
-    ADD_ATTR: ['target', 'rel']
+export const FormattedText = React.forwardRef(
+  (props: Props, ref: React.Ref<HTMLDivElement>): JSX.Element => {
+    const SANITIZE_CONFIG = {
+      ADD_ATTR: ['target', 'rel']
+    }
+
+    const EXTERNAL_TARGET = 'target="_blank"'
+    const APPENDED_TARGET = 'target="_blank" rel="noopener noreferrer"'
+
+    return (
+      <div
+        ref={ref}
+        dangerouslySetInnerHTML={{
+          __html: sanitize(
+            props.html.replace(EXTERNAL_TARGET, APPENDED_TARGET),
+            SANITIZE_CONFIG
+          )
+        }}
+        className={clsx('formatted-text', props.className)}
+      />
+    )
   }
+)
 
-  const EXTERNAL_TARGET = 'target="_blank"'
-  const APPENDED_TARGET = 'target="_blank" rel="noopener noreferrer"'
-
-  return (
-    <div
-      ref={ref} dangerouslySetInnerHTML={{
-        __html: sanitize(
-          props.html.replace(EXTERNAL_TARGET, APPENDED_TARGET),
-          SANITIZE_CONFIG
-        )
-      }}
-      className={clsx('formatted-text', props.className)}
-    />
-  )
-})
+FormattedText.displayName = 'FormattedText'
